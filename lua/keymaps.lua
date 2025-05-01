a331f6018d09a32fbdd92bf60a8f9c31bff2fe85
Edward <youuung.fella@gmail.com>
2025-05-01 18:02:32 +0500

    keymaps upgrade

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,12 +5,15 @@ map("n", " ", "<Nop>", { silent = true, remap = false })
 map('i', 'jk', '<Esc>', { noremap = true, silent = true })
 map('n', 'gd', function()
   vim.lsp.buf.definition()
-end, { buffer = bufnr, desc = 'Go to Definition (new tab)' })
+end, { buffer = bufnr, desc = 'Go to Definition' })
 
 map('n', 'gdv', function()
   vim.cmd('vsplit')
   vim.lsp.buf.definition()
 end, { buffer = bufnr })
+map('n', 'gr', function ()
+	vim.lsp.buf.references()
+end, {buffer = bufnr, desc = 'Show references'})
 vim.api.nvim_set_keymap(
   'n',
   '<leader>ca',
